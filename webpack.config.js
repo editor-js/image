20538e81f3fc831968182c8b3f8367fc363f9f63
Nikita Nafranets <eddimensi@gmail.com>
2019-04-06 12:13:50 +0300

    little update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
-          {
-            loader: 'babel-loader',
-            query: {
-              presets: [ '@babel/preset-env' ]
-            }
-          },
+          'babel-loader',
           'eslint-loader'
         ]
       },
