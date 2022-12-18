31d8f8593baac9a9b62c128a1467a20069034722
Umang G. Patel <23169768+robonetphy@users.noreply.github.com>
2022-12-19 00:52:03 +0530

    linting removed

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,18 @@
  * @author Codex Team
  * @copyright Khaydarov Murod
  */
-'use strict';
+"use strict";
 
 module.exports = (env, argv) => {
-  const path = require('path');
-  const pkg = require('./package.json');
+  const path = require("path");
+  const pkg = require("./package.json");
 
   /**
    * Environment
    *
    * @type {any}
    */
-  const NODE_ENV = argv.mode || 'development';
+  const NODE_ENV = argv.mode || "development";
   const VERSION = process.env.VERSION || pkg.version;
 
   /**
@@ -23,47 +23,47 @@ module.exports = (env, argv) => {
    *
    * @type {webpack}
    */
-  const webpack = require('webpack');
+  const webpack = require("webpack");
 
-
-    return  {
-    entry: [ './src/index.js' ],
+  return {
+    entry: ["./src/index.js"],
     module: {
-        rules: [
+      rules: [
         {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
             {
-                loader: 'babel-loader',
-                query: {
-                presets: [ '@babel/preset-env' ],
-                },
+              loader: "babel-loader",
+              query: {
+                presets: ["@babel/preset-env"],
+              },
             },
-            ]
+          ],
         },
         {
-            test: /\.css$/,
-            use: [
-            'style-loader',
-            'css-loader',
+          test: /\.css$/,
+          use: [
+            "style-loader",
+            "css-loader",
             {
-                loader: 'postcss-loader',
-                options: {
+              loader: "postcss-loader",
+              options: {
                 plugins: [
-                    require('postcss-nested-ancestors'),
-                    require('postcss-nested')
-                ]
-                }
-            }
-            ]
+                  require("postcss-nested-ancestors"),
+                  require("postcss-nested"),
+                ],
+              },
+            },
+          ],
         },
         {
-           test: /\.svg$/,
-           loader: 'svg-inline-loader?removeSVGTagAttrs=false'
-     	}]
+          test: /\.svg$/,
+          loader: "svg-inline-loader?removeSVGTagAttrs=false",
+        },
+      ],
     },
-     plugins: [
+    plugins: [
       /** Pass variables into modules */
       new webpack.DefinePlugin({
         NODE_ENV: JSON.stringify(NODE_ENV),
@@ -73,14 +73,13 @@ module.exports = (env, argv) => {
       new webpack.BannerPlugin({
         banner: `Image tool\n\n@version ${VERSION}\n\n@package https://github.com/editor-js/image\n@licence MIT\n@author CodeX <https://codex.so>`,
       }),
-     ],
+    ],
     output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js',
-        library: 'ImageTool',
-        libraryTarget: 'umd'
-    }
+      path: __dirname + "/dist",
+      publicPath: "/",
+      filename: "bundle.js",
+      library: "ImageTool",
+      libraryTarget: "umd",
+    },
   };
 };
-
