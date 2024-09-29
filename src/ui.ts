2f49db0633aada47314aec501b8e777a522513b0
Alessandro Afloarei <alessandro.afloarei@aflsolutions.it>
2024-09-29 16:20:02 +0200

    Add controls to videos.

--- src/ui.ts
+++ src/ui.ts
@@ -239,6 +239,7 @@ export default class Ui {
       attributes.loop = true;
       attributes.muted = true;
       attributes.playsinline = true;
+      attributes.controls = true;
 
       /**
        * Change event to be listened
